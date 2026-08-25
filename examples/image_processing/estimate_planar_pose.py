66d184f375d8ed632ddef61bf82d32c57fe12c01
Amrutha Venkatesan <amrutha.venkatesan@telekinesis.ai>
2026-08-25 12:38:16 +0200

    fix: minor entity path edit

--- examples/image_processing/estimate_planar_pose.py
+++ examples/image_processing/estimate_planar_pose.py
@@ -46,10 +46,10 @@ def estimate_planar_poses_example():
 
     # ===================== Visualization  (Optional) ======================
     rr.init("estimate_planar_poses_example", spawn=True)
-    datatypes.visualize(image, entity_path="0-Input Image")
+    datatypes.visualize(image, entity_path="0-InputImage")
     datatypes.visualize(mask, entity_path="1-Mask")
-    datatypes.visualize(depth_image, entity_path="2-Depth Image")
-    datatypes.visualize(pose_3d, entity_path="3-Estimated 3D Pose")
+    datatypes.visualize(depth_image, entity_path="2-DepthImage")
+    datatypes.visualize(pose_3d, entity_path="3-Estimated3DPose")
 
 if __name__ == "__main__":
     estimate_planar_poses_example()
