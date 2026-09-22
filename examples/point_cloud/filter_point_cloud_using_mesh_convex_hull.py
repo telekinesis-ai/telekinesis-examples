9e4b84ba9ad7d79f9a6c07a72b028c38598ce542
Amrutha Venkatesan <amrutha.venkatesan@telekinesis.ai>
2026-09-22 18:18:51 +0200

    fix: add media for mesh convex hull

--- examples/point_cloud/filter_point_cloud_using_mesh_convex_hull.py
+++ examples/point_cloud/filter_point_cloud_using_mesh_convex_hull.py
@@ -17,16 +17,28 @@ def filter_point_cloud_using_mesh_convex_hull_example():
     """
     # ===================== Load Data ==========================================
     point_cloud_url = (
-        "https://assets.telekinesis.ai/examples/v1/point_clouds/plastic_2_raw.ply"
+        "https://assets.telekinesis.ai/examples/v1/point_clouds/mesh_filter_example_pc.ply"
     )
-    mesh_url = "https://assets.telekinesis.ai/examples/v1/meshes/beer_can.glb"
     point_cloud = datatypes.PointCloud.from_url(url=point_cloud_url, use_cache=True)
-    mesh = datatypes.Mesh3D.from_url(url=mesh_url, use_cache=True)
+
+    mesh_url = "https://assets.telekinesis.ai/examples/v1/meshes/beer_can.glb"
+    mesh_0 = datatypes.Mesh3D.from_url(url=mesh_url, use_cache=True)
+    mesh_1 = datatypes.Mesh3D.from_url(url=mesh_url, use_cache=True)
+    mesh_2 = datatypes.Mesh3D.from_url(url=mesh_url, use_cache=True)
+    mesh_3 = datatypes.Mesh3D.from_url(url=mesh_url, use_cache=True)
+    mesh_4 = datatypes.Mesh3D.from_url(url=mesh_url, use_cache=True)
+    mesh_5 = datatypes.Mesh3D.from_url(url=mesh_url, use_cache=True)
+    mesh_6 = datatypes.Mesh3D.from_url(url=mesh_url, use_cache=True)
+    mesh_7 = datatypes.Mesh3D.from_url(url=mesh_url, use_cache=True)
+    mesh_8 = datatypes.Mesh3D.from_url(url=mesh_url, use_cache=True)
+    meshes = datatypes.Mesh3DBatch(
+        [mesh_0, mesh_1, mesh_2, mesh_3, mesh_4, mesh_5, mesh_6, mesh_7, mesh_8]
+    )
 
     # ===================== Run Skill ==========================================
     filtered_point_cloud = vitreous.filter_point_cloud_using_mesh_convex_hull(
         point_cloud=point_cloud,
-        meshes=mesh,
+        meshes=meshes,
         mode="exclude",
         padding=0.0,
         tolerance=1e-9,
@@ -36,7 +48,7 @@ def filter_point_cloud_using_mesh_convex_hull_example():
     removed_point_count = len(point_cloud.positions) - len(
         filtered_point_cloud.positions
     )
-    logger.success(f"Filtered {point_cloud} using the convex hull of {mesh}")
+    logger.success(f"Filtered {point_cloud} using the convex hulls of {meshes}")
     logger.success(f"Results: {filtered_point_cloud}")
     logger.info(
         f"Kept {len(filtered_point_cloud.positions)} points and removed "
@@ -46,7 +58,7 @@ def filter_point_cloud_using_mesh_convex_hull_example():
     # ===================== Visualization  (Optional) ===========================
     rr.init("filter_point_cloud_using_mesh_convex_hull_example", spawn=True)
     datatypes.visualize(point_cloud, entity_path="/1-input_point_cloud")
-    datatypes.visualize(mesh, entity_path="/2-filtering_mesh")
+    datatypes.visualize(meshes, entity_path="/2-filtering_meshes")
     datatypes.visualize(filtered_point_cloud, entity_path="/3-filtered_point_cloud")
 
 
