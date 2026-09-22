1e3bf369c98e17dd20bfbe47c2c6d084cfc95b09
Amrutha Venkatesan <amrutha.venkatesan@telekinesis.ai>
2026-09-22 20:22:27 +0200

    fix: updated input

--- examples/point_cloud/filter_point_cloud_using_mesh_convex_hull.py
+++ examples/point_cloud/filter_point_cloud_using_mesh_convex_hull.py
@@ -21,16 +21,16 @@ def filter_point_cloud_using_mesh_convex_hull_example():
     )
     point_cloud = datatypes.PointCloud.from_url(url=point_cloud_url, use_cache=True)
 
-    mesh_url = "https://assets.telekinesis.ai/examples/v1/meshes/beer_can.glb"
-    mesh_0 = datatypes.Mesh3D.from_url(url=mesh_url, use_cache=True)
-    mesh_1 = datatypes.Mesh3D.from_url(url=mesh_url, use_cache=True)
-    mesh_2 = datatypes.Mesh3D.from_url(url=mesh_url, use_cache=True)
-    mesh_3 = datatypes.Mesh3D.from_url(url=mesh_url, use_cache=True)
-    mesh_4 = datatypes.Mesh3D.from_url(url=mesh_url, use_cache=True)
-    mesh_5 = datatypes.Mesh3D.from_url(url=mesh_url, use_cache=True)
-    mesh_6 = datatypes.Mesh3D.from_url(url=mesh_url, use_cache=True)
-    mesh_7 = datatypes.Mesh3D.from_url(url=mesh_url, use_cache=True)
-    mesh_8 = datatypes.Mesh3D.from_url(url=mesh_url, use_cache=True)
+    mesh_0 = datatypes.Mesh3D.from_url(url="https://assets.telekinesis.ai/examples/v1/meshes/mesh_0.ply", use_cache=True)
+    mesh_1 = datatypes.Mesh3D.from_url(url="https://assets.telekinesis.ai/examples/v1/meshes/mesh_1.ply", use_cache=True)
+    mesh_2 = datatypes.Mesh3D.from_url(url="https://assets.telekinesis.ai/examples/v1/meshes/mesh_2.ply", use_cache=True)
+    mesh_3 = datatypes.Mesh3D.from_url(url="https://assets.telekinesis.ai/examples/v1/meshes/mesh_3.ply", use_cache=True)
+    mesh_4 = datatypes.Mesh3D.from_url(url="https://assets.telekinesis.ai/examples/v1/meshes/mesh_4.ply", use_cache=True)
+    mesh_5 = datatypes.Mesh3D.from_url(url="https://assets.telekinesis.ai/examples/v1/meshes/mesh_5.ply", use_cache=True)
+    mesh_6 = datatypes.Mesh3D.from_url(url="https://assets.telekinesis.ai/examples/v1/meshes/mesh_6.ply", use_cache=True)
+    mesh_7 = datatypes.Mesh3D.from_url(url="https://assets.telekinesis.ai/examples/v1/meshes/mesh_7.ply", use_cache=True)
+    mesh_8 = datatypes.Mesh3D.from_url(url="https://assets.telekinesis.ai/examples/v1/meshes/mesh_8.ply", use_cache=True)
+
     meshes = datatypes.Mesh3DBatch(
         [mesh_0, mesh_1, mesh_2, mesh_3, mesh_4, mesh_5, mesh_6, mesh_7, mesh_8]
     )
